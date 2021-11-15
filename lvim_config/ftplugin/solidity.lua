01e9d55900ca6673dd5561deaec43e257c835d9a
Radu Gafita <radu.gafita@tcnbroadcasting.com>
2021-11-15 22:15:10 +0200

    fixed wrong naming

--- lvim_config/ftplugin/solidity.lua
+++
@@ -1,9 +0,0 @@
--- https://github.com/prettier-solidity/prettier-plugin-solidity
--- npm install --save-dev prettier prettier-plugin-solidity
--- lvim.lang.solidity.formatters = {
---   {
---     exe = "prettier",
---   },
--- }
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup({{exe = "prettier", filetypes = {"solidity"} }})
