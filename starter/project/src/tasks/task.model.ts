dbd06f4975e81327e543c4d86610559cd5457fe1
Radu Gafita <radu.gafita@tcnbroadcasting.com>
2021-12-13 04:21:41 +0200

    Initial setup

---
+++ starter/project/src/tasks/task.model.ts
@@ -0,0 +1,21 @@
+export interface Task {
+  id: string;
+  title: string;
+  description: string;
+  status: TaskStatus;
+  priority: TaskPriority;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export enum TaskStatus {
+  OPEN = 'OPEN',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
+
+export enum TaskPriority {
+  LOW = 'LOW',
+  MEDIUM = 'MEDIUM',
+  HIGH = 'HIGH',
+}
---
+++ starter/project/src/tasks/tasks.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskPriority, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  public getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  public createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+      priority: TaskPriority.LOW,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+}
